bc68d48b29d1871dd0285550ae1d1ea00eaef546
Thomas Skerbis <thomas.skerbis@mac.com>
2024-09-29 11:08:26 +0200

    added webm and ogg

--- lib/video.php
+++ lib/video.php
@@ -214,7 +214,7 @@ class Video
         // Bestehenden Inhalt der Sidebar holen
         $existingContent = $ep->getSubject();
 
-        if (in_array(pathinfo($file, PATHINFO_EXTENSION), ['mp4', 'm4v', 'mov'])) {
+        if (in_array(pathinfo($file, PATHINFO_EXTENSION), ['mp4', 'm4v', 'mov', 'ogg', 'webm'])) {
             $video = new self($file);
             $video->setAttributes([
                 'crossorigin' => '',
