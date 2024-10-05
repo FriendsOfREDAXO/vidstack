e490f43d565eb90b38bf932496436122c0290b73
Thomas Skerbis <thomas.skerbis@mac.com>
2024-10-05 21:20:25 +0200

    new build

--- build/config/vs_components.js
+++ build/config/vs_components.js
@@ -1,10 +1,12 @@
 import "vidstack/player";
 import "vidstack/player/layouts/default";
-import 'vidstack/player/styles/default/layouts/audio.css';
 import "vidstack/player/ui";
-import 'media-captions/styles/captions.css';
+
 // Optional - include if rendering VTT regions.
-import 'media-captions/styles/regions.css';
+
 
 import "vidstack/player/styles/default/theme.css";
 import "vidstack/player/styles/default/layouts/video.css";
+import 'vidstack/player/styles/default/layouts/audio.css';
+import 'media-captions/styles/captions.css';
+import 'media-captions/styles/regions.css';
