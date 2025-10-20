ce5d87b956817d87cc42b3a82b004f0b87de23ff
Thomas Skerbis <thomas.skerbis@mac.com>
2025-10-20 11:06:45 +0200

    icons

--- build/config/vs_components.js
+++ build/config/vs_components.js
@@ -2,6 +2,8 @@ import 'vidstack/player';
 import 'vidstack/player/layouts/default';
 import 'vidstack/player/ui';
 
+import { defineCustomElement, MediaPosterElement } from "vidstack/elements";
+
 import 'vidstack/player/styles/default/theme.css';
 import 'vidstack/player/styles/default/layouts/video.css';
 import 'vidstack/player/styles/default/layouts/audio.css';
